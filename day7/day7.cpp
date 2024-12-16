cc2e3b43c19a748781d346b1c50f18193a03a80b
duckgii <shinyonggi@naver.com>
2024-12-16 10:26:06 +0900

    Feat : day7 end

--- day7/day7.cpp
+++ day7/day7.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-#include <vector>
+#include <queue>
 #include <fstream>
 #include <sstream>
 #include <algorithm>
@@ -7,14 +7,40 @@
 
 using namespace std;
 
+bool check(long firstNum, long retNum, queue<long> numArr)
+{
+	long	ret1 = retNum * numArr.front();
+	long	ret2 = retNum + numArr.front();
+	long	ret3 = retNum;
+	long	frontNum = numArr.front();
+
+	while (frontNum > 0)
+	{
+		ret3 *= 10;
+		frontNum /= 10;
+	}
+	ret3 += numArr.front();
+	if (numArr.size() == 1)
+	{
+		if (firstNum == ret1 || firstNum == ret2 || firstNum == ret3)
+			return (true);
+		return (false);
+	}
+	numArr.pop();
+	if (check(firstNum, ret1, numArr) || check(firstNum, ret2, numArr) || check(firstNum, ret3, numArr))
+		return (true);
+	return (false);
+}
+
 int main()
 {
 	std::ifstream	readFile;
 	std::string		filename;
 	std::string		line;
 	std::ofstream	outfile;
-	std::string	words;
-	long 	hap = 0;
+	std::string		words;
+	long 			ret = 0;
+	long 			hap = 0;
 
 
 	filename = "input.txt";
@@ -23,18 +49,29 @@ int main()
 	while (!readFile.eof())
 	{
 		std::istringstream	ss;
-		vector<int>	num;
+		long firstNum;
+		long retNum = 0;
+		bool flag = false;
+		queue<long>	numArr;
 
 		getline(readFile, line);
-		while (getline(ss, line, ' '))
+		ss.str(line);
+		getline(ss, words, ' ');
+		firstNum = atol(words.c_str());
+		getline(ss, words, ' ');
+		retNum = atoi(words.c_str());
+		while (getline(ss, words, ' '))
 		{
-			int n;
+			long n;
 			
 			n = atoi(words.c_str());
-			num.push_back(n);
+			numArr.push(n);
 		}
-		if (check())
+		if (check(firstNum, retNum, numArr))
+		{
+			ret += firstNum;
+		}
 	}
 	
-	cout<<hap<<endl;
+	cout<<ret<<endl;
 }
